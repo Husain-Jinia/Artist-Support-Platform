2c8482f787768e9238198cbb407a5f3a7d7781a6
Husain-Jinia <husain.jinia0201@gmail.com>
2021-12-11 13:43:58 +0530

    minor changes

--- core/settings.py
+++ core/settings.py
@@ -149,7 +149,7 @@ AWS_S3_FILE_OVERWRITE = False
 
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
