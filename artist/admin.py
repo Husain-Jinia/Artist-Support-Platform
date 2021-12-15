67ba3fc200125137ce70f7737445ce1dada10389
Husain-Jinia <husain.jinia0201@gmail.com>
2021-12-15 23:19:10 +0530

    minor change

--- artist/admin.py
+++ artist/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Posts, Tagname, User
+from .models import Posts, Tagname
 # Register your models here.
 
 admin.site.register(Posts)
 admin.site.register(Tagname)
-admin.site.register(User)
+# admin.site.register(User)
