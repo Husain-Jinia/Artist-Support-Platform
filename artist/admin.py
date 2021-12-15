61f7546d6709d957f3b49af77716aab40884959f
Husain-Jinia <husain.jinia0201@gmail.com>
2021-12-15 19:04:59 +0530

    minor changes

--- artist/admin.py
+++ artist/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Posts, Tagname, User
+from .models import Posts, Tagname, Users
 # Register your models here.
 
 admin.site.register(Posts)
 admin.site.register(Tagname)
-admin.site.register(User)
+admin.site.register(Users)
--- artist/models.py
+++ artist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import  datetime
 # Create your models here.
 
-class User(models.Model):
+class Users(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=12)
@@ -20,14 +20,14 @@ class User(models.Model):
     @staticmethod
     def get_customer_by_email(email):
         try:
-            return User.objects.get(email = email)
+            return Users.objects.get(email = email)
         except:
             return False
 
         
 
     def isExists(self):
-        if User.objects.filter(email = self.email):
+        if Users.objects.filter(email = self.email):
             return True
 
         return False
