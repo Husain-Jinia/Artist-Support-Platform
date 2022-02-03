ce52ac2eeb543a815d31acaad9079cb4da074e98
Husain-Jinia <husain.jinia0201@gmail.com>
2022-02-03 23:28:41 +0530

    Removing user auth feature

--- artist/models.py
+++ artist/models.py
@@ -2,35 +2,6 @@ from django.db import models
 import  datetime
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=12)
-    email = models.EmailField(max_length=500)
-    username = models.CharField(max_length=12)
-    password = models.CharField(max_length=15)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-
-    def register(self):
-        self.save()
-
-    @staticmethod
-    def get_customer_by_email(email):
-        try:
-            return User.objects.get(email = email)
-        except:
-            return False
-
-        
-
-    def isExists(self):
-        if User.objects.filter(email = self.email):
-            return True
-
-        return False
 class Tagname(models.Model):
     tag = models.CharField(max_length=100, null=True)
 
--- artist/urls.py
+++ artist/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import adminlogout, artpage, contact, home, adminpage, scrape,about, adminlogin,logout, Signup,Login
+from .views import adminlogout, artpage, contact, home, adminpage, scrape,about, adminlogin,logout
 
 urlpatterns = [
     path('', home, name="homepage"),
@@ -11,7 +11,5 @@ urlpatterns = [
     path('adminlogin', adminlogin, name="adminlogin"),
     path('adminlogout', adminlogout, name="adminlogout"),
     path('contact', contact, name='contact'),
-    path('signup', Signup.as_view(), name="signup"),
-    path('login', Login.as_view(),  name="login"),
     path('logout', logout,  name="logout"),
 ]
